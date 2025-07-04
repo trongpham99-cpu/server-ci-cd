5c1ddb5d2f300a40d5a1fe3716b22998b678cf6c
trong.phamtranduc@gmail.com <trong.phamtranduc@gmail.com>
2025-07-05 00:42:11 +0700

    init app

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello CI/CD 👋');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
